356985d736f47e4a15361d8fc76829b0dc80e59e
James Pederson <james@jpederson.com>
2024-08-09 14:33:39 +0000

    Refine ACF GUIs.

--- library/modules.php
+++ library/modules.php
@@ -32,3 +32,19 @@ function has_sidebar_nav_module() {
 
 }
 
+
+// add a wysiwyg 'short' class
+add_action('admin_head', 'acf_wysiwyg_short');
+function acf_wysiwyg_short() {
+	?>
+	<style>
+    .short .acf-editor-wrap iframe {			
+        height: 170px !important;
+    }
+    .short .mce-statusbar {
+        display: none;
+    }
+	</style>
+	<?php
+}
+
