b0095961e5af10936468f79828ef8b6cca63ae24
James Pederson <james@jpederson.com>
2026-02-02 15:23:42 +0000

    Adjust search form to include areas checkbox.

--- searchform.php
+++ searchform.php
@@ -15,7 +15,8 @@ if ( isset( $_REQUEST['post_type'] ) ) {
 	<label><span>Search Term:</span> <input type="text" value="<?php print ( isset( $_REQUEST['s'] ) ? htmlspecialchars( $_REQUEST['s'] ) : '' ); ?>" name="s" id="s" placeholder="Search"></label>
 	<label><input type="checkbox" name="post_type[]" value="page"<?php print ( in_array( 'page', $post_type ) ? ' checked="yes"' : '' ) ?> /> Pages</label>
 	<label><input type="checkbox" name="post_type[]" value="post"<?php print ( in_array( 'post', $post_type ) ? ' checked="yes"' : '' ) ?> /> Articles</label>
-	<label><input type="checkbox" name="post_type[]" value="event"<?php print ( in_array( 'event', $post_type ) ? ' checked="yes"' : '' ) ?> /> Events</label>
+	<label><input type="checkbox" name="post_type[]" value="area"<?php print ( in_array( 'area', $post_type ) ? ' checked="yes"' : '' ) ?> /> Areas of Study</label>
 	<label><input type="checkbox" name="post_type[]" value="people"<?php print ( in_array( 'people', $post_type ) ? ' checked="yes"' : '' ) ?> /> People</label>
+	<label><input type="checkbox" name="post_type[]" value="event"<?php print ( in_array( 'event', $post_type ) ? ' checked="yes"' : '' ) ?> /> Events</label>
 	<label><input type="submit" id="searchsubmit" value="Search" class="btn-arrow"></label>
 </form>
