e1b89e617dd890fd0b6d4c21b89ae6047f19608f
Jonathan Stanis <jon@jstanis.com>
2026-05-07 19:54:53 -0700

    Inital Development of Campus Map module

---
+++ modules/_campus_map.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Campus Map Module
+ * 
+ * Module partial used to display Albion Map
+ *
+ */
+ $tour_link     = get_sub_field( 'tour_link' );
+ $popup_content = get_sub_field( 'popup_content' );
+ $location      = get_sub_field( 'locations' );
+ ?>
+ 
+ 
+ 
+<?php endif; ?>
