044e429555a46bbca472859874f497c4c7ea7567
Jonathan Stanis <jon@jstanis.com>
2026-05-11 17:00:48 -0700

    Added Campus Map to ACF and modules

--- modules/_campus_map.php
+++ modules/_campus_map.php
@@ -5,11 +5,26 @@
  * Module partial used to display Albion Map
  *
  */
- $tour_link     = get_sub_field( 'tour_link' );
- $popup_content = get_sub_field( 'popup_content' );
- $location      = get_sub_field( 'locations' );
- ?>
- 
- 
- 
-<?php endif; ?>
+   $tour_link     = get_sub_field( 'tour_link' );
+   $popup_content = get_sub_field( 'popup_content' );
+   $location      = get_sub_field( 'locations' );
+?>
+<div class="map-wrapper">
+	
+	<div id="map" data-locations="<?php echo esc_attr( wp_json_encode( $location ) ); ?>"></div>
+	
+	<div class="info-wrapper">
+		<div id="info">
+			<?php 
+			  if ( $tour_link ):
+				$link_url = $tour_link['url'];
+				$link_title = $tour_link['title'];
+				$link_target = $tour_link['target'] ? $tour_link['target'] : '_self';
+				?>
+				<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+			<?php endif; ?>
+			<button id="view-map">View Albion's Map</button>
+		</div>
+	</div>
+	
+</div>
