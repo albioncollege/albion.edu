be91b9930888d6cdac3709f8ae9937b56301a469
James Pederson <james@jpederson.com>
2026-06-15 22:40:45 +0000

    Roll back changes to main container min-width.

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
       <?php get_template_part( 'modules/_footer' ); ?>
       <?php wp_footer(); ?>
-    </div> <!--- end main-container -->
     <!-- </div> end .canvas -->
       <div class="lightbox"><div class="lightbox-shade"><div class="lightbox-inner"><b class="lightbox-close">X</b><div class="lightbox-content"></div></div></div></div>
   </body>
