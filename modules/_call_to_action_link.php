cbcec70dbc9bbd59d432e2f6a2870dc3f1920703
James Pederson <james@jpederson.com>
2026-07-28 15:17:30 +0000

    Fix up call to action link module styles.

--- modules/_call_to_action_link.php
+++ modules/_call_to_action_link.php
@@ -7,12 +7,14 @@
  */
 
 if( have_rows('links') ): ?>
+<div class="cta_links">
     <p>
     <?php while ( have_rows('links') ) : the_row();
         $cta_link = get_sub_field('link');
         if ($cta_link) : ?>
-            <a href="<?php echo $cta_link['url']; ?>" class="button__link" <?php echo link_target( $cta_link ); ?>><?php echo $cta_link['title']; ?></a>
+            <div class="cta_link"><a href="<?php echo $cta_link['url']; ?>" class="button__link" <?php echo link_target( $cta_link ); ?>><?php echo $cta_link['title']; ?></a></div>
         <?php endif; ?>
     <?php endwhile;?>
     </p>
+</div>
 <?php endif; ?>
